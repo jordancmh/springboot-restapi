3837aa23d001fce62381a4887cbcb7fac15396d5
jordancmh <jordanchaymh@gmail.com>
2025-01-23 16:41:49 +0800

    Corrected project file sturcture

--- src/main/java/com/jordancmh/rest_demo/controller/CloudVendorAPIService.java
+++ src/main/java/com/jordancmh/rest_demo/controller/CloudVendorAPIService.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import model.CloudVendor;
+import com.jordancmh.rest_demo.model.CloudVendor;
 
 @RestController
 @RequestMapping("/cloudvendor")
