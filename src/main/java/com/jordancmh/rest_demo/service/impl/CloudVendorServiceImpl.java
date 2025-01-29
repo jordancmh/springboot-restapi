8176d1fd2fbfa6bbed0e7df13b03b2b390afd146
let-him-cook <jordanchaymh@gmail.com>
2025-01-29 17:09:02 +0800

    Completed implementation of service layer in CloudVendorServiceImpl

--- src/main/java/com/jordancmh/rest_demo/service/impl/CloudVendorServiceImpl.java
+++ src/main/java/com/jordancmh/rest_demo/service/impl/CloudVendorServiceImpl.java
@@ -1,5 +1,48 @@
 package com.jordancmh.rest_demo.service.impl;
 
-public class CloudVendorServiceImpl {
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.jordancmh.rest_demo.model.CloudVendor;
+import com.jordancmh.rest_demo.respository.CloudVendorRespository;
+import com.jordancmh.rest_demo.service.CloudVendorService;
+
+@Service
+public class CloudVendorServiceImpl implements CloudVendorService{
+
+    CloudVendorRespository cloudVendorRespository;
+
+    public CloudVendorServiceImpl(CloudVendorRespository cloudVendorRespository) {
+        this.cloudVendorRespository = cloudVendorRespository;
+    }
+
+    @Override
+    public String createCloudVendor(CloudVendor cloudVendor) {
+        cloudVendorRespository.save(cloudVendor);
+        return "Success";
+    }
+
+    @Override
+    public String updateCloudVendor(CloudVendor cloudVendor) {
+        cloudVendorRespository.save(cloudVendor);
+        return "Success";
+    }
+
+    @Override
+    public String deleteCloudVendor(String cloudVendorId) {
+        cloudVendorRespository.deleteById(cloudVendorId);
+        return "Success";
+    }
+
+    @Override
+    public CloudVendor getCloudVendor(String cloudVendorId) {
+        return cloudVendorRespository.findById(cloudVendorId).get();
+    }
+
+    @Override
+    public List<CloudVendor> getAllCloudVendors() {
+        return cloudVendorRespository.findAll();
+    }
 
 }
