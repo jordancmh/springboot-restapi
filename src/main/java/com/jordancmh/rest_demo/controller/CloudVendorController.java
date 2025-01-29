a5371e8c024098b4b352f025e3bb3308c7436dfa
let-him-cook <jordanchaymh@gmail.com>
2025-01-29 17:20:59 +0800

    Updated CloudVendorController to use service layer object, and renamed file appropriately.

--- src/main/java/com/jordancmh/rest_demo/controller/CloudVendorController.java
+++ src/main/java/com/jordancmh/rest_demo/controller/CloudVendorController.java
@@ -1,5 +1,7 @@
 package com.jordancmh.rest_demo.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,32 +11,44 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jordancmh.rest_demo.model.CloudVendor;
+import com.jordancmh.rest_demo.service.CloudVendorService;
 
 @RestController
 @RequestMapping("/cloudvendor")
-public class CloudVendorAPIService {
-    CloudVendor cloudVendor;
+public class CloudVendorController {
+    CloudVendorService cloudVendorService;
 
+    public CloudVendorController(CloudVendorService cloudVendorService) {
+        this.cloudVendorService = cloudVendorService;
+    }
+
+    // Read specific Cloud Vendor details from DB
     @GetMapping("{vendorId}")
     public CloudVendor getCloudVendorDetails(String vendorId) {
-        return cloudVendor;
+        return cloudVendorService.getCloudVendor(vendorId);
+    }
+    
+    // Read ALL Cloud Vendor details from DB
+    @GetMapping()
+    public List<CloudVendor> getAllCloudVendorDetails() {
+        return cloudVendorService.getAllCloudVendors();
     }
 
     @PostMapping
     public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor) {
-        this.cloudVendor = cloudVendor;
+        cloudVendorService.createCloudVendor(cloudVendor);
         return "Cloud Vendor Created Successfully";
     }
 
     @PutMapping
     public String updateCloudVendorDetails(@RequestBody CloudVendor cloudVendor) {
-        this.cloudVendor = cloudVendor;
+        cloudVendorService.updateCloudVendor(cloudVendor);
         return "Cloud Vendor Updated Successfully";
     }
 
     @DeleteMapping("{vendorId}")
     public String deleteCloudVendorDetails(String vendorId) {
-        this.cloudVendor = null;
+        cloudVendorService.deleteCloudVendor(vendorId);
         return "Cloud Vendor Deleted Successfully";
     }
 }
